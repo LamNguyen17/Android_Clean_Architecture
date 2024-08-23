f55ee13a1ebd78ceb866f140d6fcd953d9cad508
Forest Nguyen <devlamnt176@gmail.com>
2024-08-23 22:28:38 +0700

    add strings(#navigation_flow)

---
+++ app/src/main/java/com/forest/android_clean_architecture/di/AuthModule.kt
@@ -0,0 +1,17 @@
+package com.forest.android_clean_architecture.di
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+}
